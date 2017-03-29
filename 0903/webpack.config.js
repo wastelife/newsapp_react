9e9569dce3aa90cde8b306ea81f4fb46b2d7322d
ww <ww_1125@outlook.com>
2017-03-29 14:02:33 -0700

    CSS模块化

---
+++ 0903/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  context: __dirname + '/src',
+  entry: "./js/index.js",
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['react-html-attrs'], //添加组件的插件配置
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+      },
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "bundle.js"
+  },
+};
