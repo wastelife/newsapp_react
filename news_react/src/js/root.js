7a20daf653657a828526be254627e44fefc39cf6
ww <ww_1125@outlook.com>
2017-04-02 22:24:26 -0700

    Add detail page & comments function & favorite function

--- news_react/src/js/root.js
+++ news_react/src/js/root.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {Button} from 'antd';
 import PCIndex from './components/pc_index';
+import PCNewsDetails from './components/pc_news_details';
+import MobileNewsDetails from './components/mobile_news_details';
 import MobileIndex from './components/mobile_index';
 import 'antd/dist/antd.css';
 import MediaQuery from 'react-responsive';
@@ -11,10 +13,16 @@ export default class Root extends React.Component {
 		return (
 			<div>
 				<MediaQuery query='(min-device-width: 1224px)'>
-					<PCIndex/>
+					<Router history={hashHistory}>
+						<Route path="/" component={PCIndex}></Route>
+						<Route path="/details/:uniquekey" component={PCNewsDetails}></Route>
+					</Router>
 				</MediaQuery>
 				<MediaQuery query='(max-device-width: 1224px)'>
-					<MobileIndex/>
+					<Router history={hashHistory}>
+						<Route path="/" component={MobileIndex}></Route>
+						<Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
+					</Router>
 				</MediaQuery>
 			</div>
 		);
